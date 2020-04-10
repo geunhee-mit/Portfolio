04110818ae9a94fc43ca6202b95b7096c80e0fd3
geunhee <lee.geunheee@gmail.com>
2020-04-10 19:34:45 -0400

    20200410
    initial commit

---
+++ js/main.js
@@ -0,0 +1,113 @@
+/*
+* Template Name: MyPortfolio
+* Template URL: https://bootstrapmade.com/myportfolio-bootstrap-portfolio-website-template/
+* License: https://bootstrapmade.com/license/
+*/
+
+(function ($) {
+  "use strict";
+
+  var burgerMenu = function() {
+	  $('.burger').click(function(e) {
+	  	$(window).scrollTop(0);
+	    if(!$('.burger').hasClass('active'))
+	      $('.burger').addClass('active');
+	    else
+	      $('.burger').removeClass('active');
+	  });
+  }
+  burgerMenu();
+
+  var siteIstotope = function() {
+	  var $container = $('#portfolio-grid').isotope({
+	    itemSelector : '.item',
+	    isFitWidth: true
+	  });
+
+	  $(window).resize(function(){
+	    $container.isotope({
+	      columnWidth: '.col-sm-3'
+	    });
+	  });
+
+	  $container.isotope({ filter: '*' });
+
+	  $('#filters').on( 'click', 'a', function(e) {
+	  	e.preventDefault();
+	    var filterValue = $(this).attr('data-filter');
+	    $container.isotope({ filter: filterValue });
+	    $('#filters a').removeClass('active');
+	    $(this).addClass('active');
+	  });
+  }
+  $(window).on('load', function () {
+    siteIstotope();
+  });
+
+
+  var siteOwlCarousel = function() {
+  	$('.testimonial-carousel').owlCarousel({
+		  center: true,
+	    items: 1,
+	    loop: true,
+	    margin: 0,
+	    autoplay: true,
+	    smartSpeed: 500,
+		});
+  };
+  siteOwlCarousel();
+
+
+})(jQuery);
+
+AOS.init({
+	easing: 'ease',
+	duration: 1000,
+	once: true
+});
+
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 54)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 56
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+})(jQuery); // End of use strict
+
+
+$('.carousel').carousel()
